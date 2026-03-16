e7b14882dea2bf2d3e0b5158bae15721a3caf829
hosanareis <hosanareis@hotmail.com>
2026-03-16 18:02:05 -0300

    ajuste no texto do site

--- scripts.js
+++ scripts.js
@@ -11,12 +11,12 @@ const mascara = document.querySelector(".mascara-formulario")
  
 function mostrarForm(){
     form.style.left = "50%"
-    form.style.transform = "translatex(-50%)"
+    form.style.transform = "translateX(-50%)"
     mascara.style.visibility = "visible"
 }
 
 function esconderForm(){
     form.style.left = "-300px"
-    form.style.transform = "translatex(0)"
+    form.style.transform = "translateX(0)"
     mascara.style.visibility = "hidden"
 }
